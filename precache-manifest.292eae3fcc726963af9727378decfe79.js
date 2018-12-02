265147ea003d04e9f712a7385ab8bca2ed9342b3
Floyd Chen <chen2918@umn.edu>
2018-12-02 00:03:23 -0600

    Updates

---
+++ precache-manifest.292eae3fcc726963af9727378decfe79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3adbf625ba31ab514d5b",
+    "url": "/static/css/main.8eb08f91.chunk.css"
+  },
+  {
+    "revision": "3adbf625ba31ab514d5b",
+    "url": "/static/js/main.3adbf625.chunk.js"
+  },
+  {
+    "revision": "3bf8d718a66535446819",
+    "url": "/static/js/1.3bf8d718.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae49440c7677cfeabc130d443610309d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/floyd-resume/precache-manifest.e42644a8fc66012e9780d9cddae3e731.js"
+  "/precache-manifest.292eae3fcc726963af9727378decfe79.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/floyd-resume/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
